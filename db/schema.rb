25a87799cd8f0d1dbc51c0f0cf64511e6fb847ef
EC2 Default User <ec2-user@ip-172-31-93-212.ec2.internal>
2019-11-19 04:42:47 +0000

    Image Uploader and comment on image

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_035954) do
+ActiveRecord::Schema.define(version: 2019_11_19_042255) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.integer "number_of_pages"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "image_comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "image_id"
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.string "file"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
